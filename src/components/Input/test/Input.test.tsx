68e21e577de27daeefebb2054ccde0d520a25837
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-10-24 21:30:46 +0900

    Input component testing: add beforeEach

--- src/components/Input/test/Input.test.tsx
+++ src/components/Input/test/Input.test.tsx
@@ -4,8 +4,12 @@ import '@testing-library/jest-dom';
 import Input from '../Input';
 
 describe('Input component testing', () => {
-    test('renders the input field and add icon', () => {
+    // 각 테스트 케이스가 실행되기 전에 Input 컴포넌트를 렌더링
+    beforeEach(() => {
         render(<Input />);
+    });
+
+    test('renders the input field and add icon', () => {
         const inputElement = screen.getByPlaceholderText('추가하세요...');
         const addIconElement = screen.getByAltText('send');
 
@@ -13,11 +17,10 @@ describe('Input component testing', () => {
         expect(addIconElement).toBeInTheDocument();
     });
 
-    test('allow to type a todo item', () => {
-        render(<Input />);
+    test('type in inputField', () => {
         const inputField = screen.getByPlaceholderText('추가하세요...') as HTMLInputElement;
-
-        fireEvent.change(inputField, { target: { value: 'something' } }); // 'something'을 입력하면 inputField의 value가 'something'이 되는지 확인
+        // 'something'을 입력하면 inputField의 value가 'something'이 되는지 확인
+        fireEvent.change(inputField, { target: { value: 'something' } });
         expect(inputField.value).toBe('something');
     });
 });
